7971342b95f2ee37b4fdf23a492673095d9eaa33
William Metachu Chu <chudood@gmail.com>
2012-04-01 12:28:06 +1000

    modified for github

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ import os
 
 HOST = '0.0.0.0'
 PORT = '8030'
-STATIC_URL = 'http://www.metachu.com:9092/downloads/'
-STATIC_LOCAL = '/home/chu/'
+STATIC_URL = 'http://somewebsite.com/downloads/''
+STATIC_LOCAL = '/some/local/downloads/folder/'
 STATIC_LOCAL = os.path.abspath(STATIC_LOCAL)
 
-subdir = "/"
+subdir = "/index/"  #web_route needs to be surrounded by 2 / /
 DATE_FORMAT = "%d-%b-%y" #30-Mar-2012
